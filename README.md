89fdd7c5d079fc05bc791428552e089403180116
Intro1997 <29827323+Intro1997@users.noreply.github.com>
2026-02-14 19:41:19 +0800

    feat: init repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# 💤 LazyVim
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+Run the command `git clone https://github.com/LazyVim/starter ~/.config/nvim` in terminal for Linux/macOS
+
+
---
+++ lua/plugins/xcode_project_config.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "wojciech-kulik/xcodebuild.nvim",
+    dependencies = {
+      -- Uncomment a picker that you want to use, snacks.nvim might be additionally
+      -- useful to show previews and failing snapshots.
+
+      -- You must select at least one:
+      -- "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
+      "folke/snacks.nvim", -- (optional) to show previews
+
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-tree.lua", -- (optional) to manage project files
+      "stevearc/oil.nvim", -- (optional) to manage project files
+      "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+    },
+    config = function()
+      require("xcodebuild").setup({
+        -- put some options here or leave it empty to use default settings
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        sourcekit = {
+          cmd = { "sourcekit-lsp" },
+          filetypes = { "swift" },
+          root_markers = {
+            "buildServer.json",
+            ".git",
+          },
+        },
+      },
+    },
+  },
+}
+-- If you first run this config,you should also run following command in terminal:
+-- cd ~/.local/share/nvim/lazy/xcodebuild.nvim
+-- make install
