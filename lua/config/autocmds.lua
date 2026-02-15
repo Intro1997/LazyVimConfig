ab7089bcc04786fd116bba4761392fe667d6937e
Intro1997 <29827323+Intro1997@users.noreply.github.com>
2026-02-15 19:23:33 +0800

    fix: move ui config to autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,7 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#FF966C" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#FF966C" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FF966C" })
--- lua/plugins/ui_config.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_hl(0, "LineNrBefore", { fg = "#FF966C" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#FF966C" })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FF966C" })
