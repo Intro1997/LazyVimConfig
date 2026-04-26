ab59a7091b8ad883ed7d7e322eb9a3abceb73ab7
Intro1997 <29827323+Intro1997@users.noreply.github.com>
2026-04-27 01:13:16 +0800

    feat: close copy when press 'd', let 'd' only be used to delete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- d options will cut current content not delete,
+-- it will cover origin copied content, follwing
+-- setting will replace d with _d, which destroy
+-- content register
+vim.keymap.set({ "n", "v" }, "d", '"_d')
