f64afe4206fd54e20c128d9472c2acb01f390304
Intro1997 <29827323+Intro1997@users.noreply.github.com>
2026-05-14 13:53:57 +0800

    feat: update markdown config
    1. remove markdown lint
    2. add table format

--- lua/plugins/markdown_format.lua
+++ lua/plugins/markdown_format.lua
@@ -1,13 +1,39 @@
 return {
+  -- 1. 确保 Mason 安装了 prettier
   {
-    "mason-org/mason.nvim",
-    opts = { ensure_installed = { "prettier" } },
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "prettier")
+    end,
   },
+
+  -- 2. 配置 Conform 强制运行 Prettier
   {
     "stevearc/conform.nvim",
+    opts = function(_, opts)
+      -- 定义格式化器行为
+      opts.formatters = opts.formatters or {}
+      opts.formatters.prettier = {
+        -- 核心修复：强制 Prettier 运行，即使没有 .prettierrc 配置文件
+        condition = function()
+          return true
+        end,
+      }
+
+      -- 设置不同文件类型使用的格式化器
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft["markdown"] = { "prettier", "markdown-toc" }
+      opts.formatters_by_ft["markdown.mdx"] = { "prettier", "markdown-toc" }
+    end,
+  },
+
+  -- 3. 禁用不需要的 linter
+  {
+    "mfussenegger/nvim-lint",
     opts = {
-      markdownlint = {
-        args = { "--config", os.getenv("HOME") .. "/.markdownlint.json", "--fix", "$FILENAME" },
+      linters_by_ft = {
+        markdown = {},
       },
     },
   },
